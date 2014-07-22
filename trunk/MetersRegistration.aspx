69e687541e953fac7c692a1a80b7e5ceb06e800b
Denis.Bredikhin@gmail.com <Denis.Bredikhin@gmail.com@100501c1-e033-6d21-69ca-a386bb692957>
2014-07-22 07:15:21 +0000

    - Added webpage for meters registration

---
+++ trunk/MetersRegistration.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MetersRegistration.aspx.cs" Inherits="HomeDashboard.MetersRegistration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Регистрация показаний счетчиков</title>
+    <link href='Styles/Common.css' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Газ:        
+    </div>
+    <asp:TextBox runat="server" ID="gas"></asp:TextBox>
+    
+    <div>
+        Эл-во день:        
+    </div>
+    <asp:TextBox runat="server" ID="electrDay"></asp:TextBox>
+    
+    <div>
+        Эл-во ночь:        
+    </div>
+    <asp:TextBox runat="server" ID="electrNight"></asp:TextBox>
+    
+    <div>
+        Вода:        
+    </div>
+    <asp:TextBox runat="server" ID="water"></asp:TextBox>
+    
+    <br><br>
+        <asp:Button runat="server" OnClick="okButton_Click" ID="okButton" Text="Зарегистрировать" />
+        
+        <div style="text-align:center;margin-top:20px;width:100%">
+         <a class="btn" href="Menu.html">Назад</a>
+        </div>
+    </form>
+</body>
+</html>
---
+++ trunk/MetersRegistration.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HomeDashboard
+{
+	public partial class MetersRegistration : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			//ScriptManager.ScriptResourceMapping.AddDefinition("jquery",
+			//new ScriptResourceDefinition {
+			//	Path = "~/scripts/jquery-2.1.0.min.js",
+			//	DebugPath = "~/scripts/jquery-2.1.0.js",
+			//	CdnPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.0.min.js",
+			//	CdnDebugPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.0.js"
+			//});
+
+		}
+
+		protected void okButton_Click(object sender, EventArgs e)
+		{
+			var now = DateTime.Today;
+			var commandText = string.Format(
+				"INSERT INTO [Meters]" +
+					" ([Date], [Gas], [ElectricityDay], [ElectricityNight], [Water])" +
+				" VALUES" +
+				" (@date, @gas, @electrDay, @electrNight, @water)");
+			var maxDateCommandText = "SELECT MAX([Date]) FROM [Meters]";
+
+			using (var connection = new SqlConnection("Server=localhost;Database=HomeData;Trusted_Connection=True;")) {
+				try {
+					connection.Open();
+					DateTime lastDate = now;
+					using (var command = connection.CreateCommand()) {
+						command.CommandText = maxDateCommandText;
+
+						lastDate = (DateTime)command.ExecuteScalar();
+					}
+
+					if ((now.Date-lastDate.Date).TotalDays>1) {
+						for (var d = lastDate.Date.AddDays(1); d.Date<now.Date;d = d.AddDays(1)) {
+							using (var command = connection.CreateCommand()) {
+								command.CommandText = commandText;
+								command.Parameters.AddWithValue("@date", d);
+								command.Parameters.AddWithValue("@gas", DBNull.Value);
+								command.Parameters.AddWithValue("@electrDay", DBNull.Value);
+								command.Parameters.AddWithValue("@electrNight", DBNull.Value);
+								command.Parameters.AddWithValue("@water", DBNull.Value);
+								command.ExecuteNonQuery();
+							}
+						}
+					}
+
+					using (var command = connection.CreateCommand()) {
+						command.CommandText = commandText;
+						command.Parameters.AddWithValue("@date", now);
+						command.Parameters.AddWithValue("@gas", double.Parse(gas.Text));
+						command.Parameters.AddWithValue("@electrDay", double.Parse(electrDay.Text));
+						command.Parameters.AddWithValue("@electrNight", double.Parse(electrNight.Text));
+						command.Parameters.AddWithValue("@water", double.Parse(water.Text));
+						command.ExecuteNonQuery();
+					}
+				}
+				finally {
+ 					if (connection.State==ConnectionState.Open)
+						connection.Close(); 
+					connection.Dispose();
+				}
+			}
+		}
+	}
+}
